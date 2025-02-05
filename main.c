9e359c112ef4dc5e6bd2ac3a4635419114bce4ab
SollyCB <solomoncardenbrown@gmail.com>
2025-02-05 18:16:50 +0000

    positive exp seems to work

--- main.c
+++ main.c
@@ -16,66 +16,70 @@ static u64 powu64(u64 x, u64 e)
 
 int main() {
 
-//	float f = 3.998989;
-//	int x = *(int*) &f;
-	int x = 0b0100'0000'0100'0110'1100'0000'0000'0000;
-//	int x = 0b0011'1111'1111'1110'0000'0000'0000'0000;
+	float f = 8.1782;
+	int x = *(int*) &f;
+//	int x = 0b0100'0000'0000'0110'1100'0000'0000'0000;
+//	int x = 0b0100'0000'0000'1110'0000'0000'0000'0000;
 	printf("glibc = %.6f\n", *(float*) &x);
 
-	int e = ((x & (0b0111'1111'1 << 23)) >> 23) - 127;
-	printf("e = %i\n", e);
+	int exp = ((x & (0b0111'1111'1 << 23)) >> 23) - 127;
+	printf("exp = %i\n", exp);
 
 	u64 a[p] = {};
 	int b = 22;
 	for (int i=1; i <= p; ++i) {
 		a[i-1] = powu64(5, i) * (bool)(x & (1 << b));
 		--b;
-//		printf("a[%i] = %lu\n", i, a[i-1]);
 	}
 
 	u64 q = 0;
-	int r[p] = {};
-	for(int i = p-1; i >= 0; --i) {
-		printf("acc = %lu -> %lu, a[%i] = %lu, x = %lu\n",
-			q, q + a[i], i, a[i], (q + a[i]) % 10);
+	int r[64] = {},s=-1,e=0;
+	for(int i = p-1; i >= 0; --i,++e) {
 		q += a[i];
 		r[p-i-1] = q % 10;
+		printf("r[%i] = %i\n", p-i-1, r[p-i-1]);
 		q /= 10;
+		if (r[p-i-1] && s == -1)
+			s = p-i-1;
 	}
-	for(int i=0; i < p; ++i)
-		printf("r[%i] = %i\n", i, r[i]);
 	printf("\n");
 
-	// 0 : ctz % 4 == 0 && ctz == 0
-	// 2 : ctz % 4 == 1
-	// 4 : ctz % 4 == 2
-	// 8 : ctz % 4 == 3
-	// 6 : ctz % 4 == 0 && ctz > 0
+	r[e++] = 1;
 
-//	int tab[] = {2, 4, 8, 6};
-	int pos = e > 0 ? e % 4 : 0;
-
-	// Keep taking mod 10, and dividing by 10, and correcting for the true
-	// bit shift by knowing what the what the bit shift is without actually
-	// doing it.
-
-	int s[26] = {};
-	bool c = 0;
-	for(int i=0; i < p; ++i) {
-		printf("pos = %i, r[%i] = %i, ", pos, i, r[i]);
-		r[i] <<= pos;
-		s[i] = (r[i] + c) % 10;
-		c = (r[i] >> 1) % 10 >= 5;
-		printf("s[%i] = %i, carry = %i\n", i, s[i], c);
+	int pos = e;
+	bool c=0;
+	for(int i=0; i < exp; ++i) {
+		for(int j=s; j <= e; ++j) {
+			int tmp = r[j];
+			r[j] = (r[j] * 2 % 10) + c;
+			c = tmp > 4;
+			printf("r[%i] = %i\n", j, r[j]);
+		}
+		s += r[s] == 0 || r[s] == 5;
+		e += r[e] > 0;
+		printf("s = %i, e = %i\n", s, e);
 	}
 
+#if 1
+	printf("round = %i\n", r[e-8]);
+	c = r[e-8] > 4;
+	for(int i=e-7; c; ++i) {
+		int tmp = r[i];
+		r[i] += c;
+		c = tmp == 9;
+	}
+#endif
+
+	printf("\nglibc = %.6f\n", *(float*) &x);
+
+	printf("sol   = ");
+	while(e-- >= pos)
+		printf("%i", r[e]);
+	printf(".");
+
+	for(int i=e; i > e-6; --i)
+		printf("%i", r[i]);
 	printf("\n");
-	printf("e = %i\n", e);
-	printf("%li.", powu64(2, e));
-	for(int i=p-1; i >= p-6; --i)
-		printf("%i", s[i]);
-	printf("\n");
-	printf("glibc = %.6f\n", *(float*) &x);
 
 	return 0;
 }
